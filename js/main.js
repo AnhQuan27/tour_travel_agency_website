4ff80672a0dcb9bb3d3bbc6967f2a8a1513191c9
Quân <otabe2703@gmail.com>
2023-10-23 20:17:59 +0700

    Login/Register UI

--- js/main.js
+++ js/main.js
@@ -50,3 +50,65 @@ function getNumTravelers() {
 }
 
 getNumTravelers();
+
+function showModal() {
+  const modal = document.querySelector('.modal');
+  const forgot = document.getElementById('forgot');
+  forgot.addEventListener('click', function (event) {
+      event.preventDefault();
+  modal.classList.remove('disable');
+  });
+}
+
+showModal();
+
+// function setRememberMe() {
+//   const rememberMeCheckbox = document.getElementById('remember_me');
+//   const username = document.getElementById('account').value;
+//   const password = document.getElementById('password').value;
+
+//   if (rememberMeCheckbox.checked) {
+//       // Lưu thông tin đăng nhập vào cookies
+//       document.cookie = `rememberedUsername=${username}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
+//       document.cookie = `rememberedPassword=${password}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
+
+//       // Hoặc lưu thông tin đăng nhập vào local storage
+//       localStorage.setItem('rememberedUsername', username);
+//       localStorage.setItem('rememberedPassword', password);
+//   }
+// }
+
+// // Hàm để kiểm tra và điền thông tin nếu có lưu trữ
+// function checkRememberMe() {
+//   // Kiểm tra xem có thông tin đã được lưu trữ trong cookies
+//   let rememberedUsernameCookie = getCookie('rememberedUsername');
+//   let rememberedPasswordCookie = getCookie('rememberedPassword');
+
+//   // Hoặc kiểm tra xem có thông tin đã được lưu trữ trong local storage
+//   let rememberedUsernameLocal = localStorage.getItem('rememberedUsername');
+//   let rememberedPasswordLocal = localStorage.getItem('rememberedPassword');
+
+//   // Ghi nhớ thông tin
+//   let rememberedUsername, rememberedPassword;
+
+//   // Sử dụng thông tin từ cookies nếu tồn tại
+//   if (rememberedUsernameCookie && rememberedPasswordCookie) {
+//       rememberedUsername = rememberedUsernameCookie;
+//       rememberedPassword = rememberedPasswordCookie;
+//   }
+
+//   // Sử dụng thông tin từ local storage nếu tồn tại và không có thông tin từ cookies
+//   else if (rememberedUsernameLocal && rememberedPasswordLocal) {
+//       rememberedUsername = rememberedUsernameLocal;
+//       rememberedPassword = rememberedPasswordLocal;
+//   }
+
+//   // Nếu có thông tin lưu trữ, điền nó vào các trường đăng nhập
+//   if (rememberedUsername && rememberedPassword) {
+//       document.getElementById('usernameInput').value = rememberedUsername;
+//       document.getElementById('passwordInput').value = rememberedPassword;
+//       document.getElementById('rememberMeCheckbox').checked = true;
+//   }
+// }
+
+// checkRememberMe();
